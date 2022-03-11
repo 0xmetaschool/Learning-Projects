47712b33af01738c7ed4caf03f1dbdcaf9cd9f4e
YousraMashkoor <yousra.mashkoor@gmail.com>
2022-03-11 09:22:08 +0500

    deploying contract

---
+++ Writing your first Hello World contract in Solidity/Code/contracts/HelloWorld.sol
@@ -0,0 +1,23 @@
+pragma solidity >= 0.7.3;
+
+contract HelloWorld {
+    //events
+    //states
+    //functions
+ 
+    event messagechanged(string oldmsg, string newmsg);
+ 
+    string public message;
+ 
+    constructor(string memory firstmessage) {
+        message = firstmessage;   
+    }
+ 
+    function update(string memory newmesssage) public {
+        string memory oldmsg = message;
+        message = newmesssage;
+ 
+        emit messagechanged(oldmsg, newmesssage);
+ 
+    }
+}
